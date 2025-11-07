e85667daa712c22bff7586b50d9ead6c5285a30c
Paula Melo <paulamelo2404@gmail.com>
2025-11-07 12:14:09 -0300

    Feat: correção de roda axios baseURL

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://safenest-swagger.onrender.com",
 });
 
 export const cadastrarUsuario = async (
